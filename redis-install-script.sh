53d1d1f9c1c3585bbeee951134fc7579a5a89ff3
messick <nmessick@gmail.com>
2013-04-19 10:45:18 -0600

    modifying to fit our needs

--- redis-install-script.sh
+++ redis-install-script.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+# Git Repo Forked from: https://github.com/saxenap/install-redis-amazon-linux-centos 
+#
 # From here: http://www.codingsteps.com/install-redis-2-6-on-amazon-ec2-linux-ami-or-centos/
 # Thanks to https://raw.github.com/gist/2776679/b4f5f5ff85bddfa9e07664de4e8ccf0e115e7b83/install-redis.sh
-# Uses redis-server init script from https://raw.github.com/saxenap/install-redis-amazon-linux-centos/master/redis-server
+# Uses redis-server init script from https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
 ###############################################
 # To use: 
-# wget https://raw.github.com/saxenap/install-redis-amazon-linux-centos/master/redis-install-script.sh
+# wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-install-script.sh
 # chmod 777 redis-install-script.sh
 # ./redis-install-script.sh
 ###############################################
 echo "*****************************************"
-echo " 1. Prerequisites: Install updates, set time zones, install GCC and make"
+echo " 1. Prerequisites: Install updates, install GCC and make"
 echo "*****************************************"
 sudo yum -y update
-sudo ln -sf /usr/share/zoneinfo/America/Indianapolis /etc/localtime
 sudo yum -y install gcc gcc-c++ make 
 echo "*****************************************"
 echo " 2. Download, Untar and Make Redis 2.6"
 echo "*****************************************"
-sudo wget http://redis.googlecode.com/files/redis-2.6.0-rc3.tar.gz
-sudo tar xzf redis-2.6.0-rc3.tar.gz
-sudo rm redis-2.6.0-rc3.tar.gz -f
-cd redis-2.6.0-rc3
+sudo wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
+sudo tar xzf redis-2.6.12.tar.gz
+sudo rm redis-2.6.12.tar.gz -f
+cd redis-2.6.12
 sudo make
 sudo make install
 echo "*****************************************"
@@ -43,7 +44,7 @@ sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.
 echo "*****************************************"
 echo " 5. Download init Script"
 echo "*****************************************"
-wget https://raw.github.com/saxenap/install-redis-amazon-linux-centos/master/redis-server
+wget https://messick/saxenap/install-redis-amazon-linux-centos/master/redis-server
 echo "*****************************************"
 echo " 6. Move and Configure Redis-Server"
 echo "*****************************************"
@@ -60,11 +61,6 @@ echo "*****************************************"
 sudo service redis-server start
 echo "*****************************************"
 echo " Installation Complete!"
-echo " You can test your redis installation using the redis console:"
-echo "   $ src/redis-cli"
-echo "   redis> ping"
-echo "   PONG"
-echo "*****************************************"
 echo " Following changes have been made in redis.config:"
 echo " 1: ... daemonize yes"
 echo " 2: ... bind 127.0.0.1"
