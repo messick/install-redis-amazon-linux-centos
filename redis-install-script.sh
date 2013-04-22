ccbb7be1547c59f5d7dc3cad97726518fdc11998
messick <nmessick@gmail.com>
2013-04-22 10:52:10 -0600

    remove fluff, add sidekiq config stuff

--- redis-install-script.sh
+++ redis-install-script.sh
@@ -1,72 +1,38 @@
 #!/bin/bash
-# Git Repo Forked from: https://github.com/saxenap/install-redis-amazon-linux-centos 
-#
-# From here: http://www.codingsteps.com/install-redis-2-6-on-amazon-ec2-linux-ami-or-centos/
-# Thanks to https://raw.github.com/gist/2776679/b4f5f5ff85bddfa9e07664de4e8ccf0e115e7b83/install-redis.sh
-# Uses redis-server init script from https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
-###############################################
-# To use: 
-# wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-install-script.sh
-# chmod +x redis-install-script.sh
-# ./redis-install-script.sh
-###############################################
-echo "*****************************************"
-echo " 1. Prerequisites: Install updates, install GCC and make"
-echo "*****************************************"
+
 sudo yum -y update
 sudo yum -y install gcc gcc-c++ make 
-echo "*****************************************"
-echo " 2. Download, Untar and Make Redis 2.6"
-echo "*****************************************"
+
 sudo wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
 sudo tar xzf redis-2.6.12.tar.gz
 sudo rm redis-2.6.12.tar.gz -f
+
 cd redis-2.6.12
 sudo make
 sudo make install
-echo "*****************************************"
-echo " 3. Create Directories and Copy Redis Files"
-echo "*****************************************"
+
 sudo mkdir /etc/redis /var/lib/redis
 sudo cp src/redis-server src/redis-cli /usr/local/bin
 sudo cp redis.conf /etc/redis
-echo "*****************************************"
-echo " 4. Configure Redis.Conf"
-echo "*****************************************"
-echo " Edit redis.conf as follows:"
-echo " 1: ... daemonize yes"
-echo " 2: ... bind 127.0.0.1"
-echo " 3: ... dir /var/lib/redis"
-echo " 4: ... loglevel notice"
-echo " 5: ... logfile /var/log/redis.log"
-echo "*****************************************"
+
 sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
-echo "*****************************************"
-echo " 5. Download init Script"
-echo "*****************************************"
+
 wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
-echo "*****************************************"
-echo " 6. Move and Configure Redis-Server"
-echo "*****************************************"
+
 sudo mv redis-server /etc/init.d
 sudo chmod 755 /etc/init.d/redis-server
-echo "*****************************************"
-echo " 7. Auto-Enable Redis-Server"
-echo "*****************************************"
+
 sudo chkconfig --add redis-server
 sudo chkconfig --level 345 redis-server on
-echo "*****************************************"
-echo " 8. Start Redis Server"
-echo "*****************************************"
-sudo service redis-server start
-echo "*****************************************"
-echo " Installation Complete!"
-echo " Following changes have been made in redis.config:"
-echo " 1: ... daemonize yes"
-echo " 2: ... bind 127.0.0.1"
-echo " 3: ... dir /var/lib/redis"
-echo " 4: ... loglevel notice"
-echo " 5: ... logfile /var/log/redis.log"
-echo "*****************************************"
-read -p "Press [Enter] to continue..."
 
+sudo service redis-server start
+
+wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/sidekiq
+
+sudo mv sidekiq /etc/init.d
+sudo chmod 755 --level/init.d/sidekiq
+
+sudo chkconfig --add sidekiq
+sudo chkconfig --level 345 sidekiq on
+
+sudo service sidekiq start
