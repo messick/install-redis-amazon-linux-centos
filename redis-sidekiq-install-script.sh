5309ff0ee568292a2d74cd28911f7b806a4bc72c
Bob Bonifield <bobbonifield@gmail.com>
2013-04-24 10:35:09 -0600

    Cleanup extra step

--- redis-sidekiq-install-script.sh
+++ redis-sidekiq-install-script.sh
@@ -10,9 +10,6 @@ make
 make install
 
 mkdir /etc/redis /var/lib/redis
-cp src/redis-server src/redis-cli /usr/local/bin
-cp redis.conf /etc/redis
-
 sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
 
 
@@ -30,7 +27,7 @@ service redis-server start
 # add sidekiq as a service
 wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/sidekiq
 mv sidekiq /etc/init.d
-chmod 755 --level/init.d/sidekiq
+chmod 755 /etc/init.d/sidekiq
 
 chkconfig --add sidekiq
 chkconfig --level 345 sidekiq on
