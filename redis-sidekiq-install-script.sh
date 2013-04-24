3ba34f2f8fdd89c26e51ab18d34144f7e4ec84e7
Bob Bonifield <bobbonifield@gmail.com>
2013-04-24 10:25:08 -0600

    Remove unnecessary sudo's

--- redis-sidekiq-install-script.sh
+++ redis-sidekiq-install-script.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # install and setup redis
-sudo wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
-sudo tar xzf redis-2.6.12.tar.gz
-sudo rm redis-2.6.12.tar.gz -f
+wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
+tar xzf redis-2.6.12.tar.gz
+rm redis-2.6.12.tar.gz -f
 
 cd redis-2.6.12
-sudo make
-sudo make install
+make
+make install
 
-sudo mkdir /etc/redis /var/lib/redis
-sudo cp src/redis-server src/redis-cli /usr/local/bin
-sudo cp redis.conf /etc/redis
+mkdir /etc/redis /var/lib/redis
+cp src/redis-server src/redis-cli /usr/local/bin
+cp redis.conf /etc/redis
 
-sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
+sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
 
 
 # add redis as a service
 wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
-sudo mv redis-server /etc/init.d
-sudo chmod 755 /etc/init.d/redis-server
+mv redis-server /etc/init.d
+chmod 755 /etc/init.d/redis-server
 
-sudo chkconfig --add redis-server
-sudo chkconfig --level 345 redis-server on
+chkconfig --add redis-server
+chkconfig --level 345 redis-server on
 
-sudo service redis-server start
+service redis-server start
 
 
 # add sidekiq as a service
 wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/sidekiq
-sudo mv sidekiq /etc/init.d
-sudo chmod 755 --level/init.d/sidekiq
+mv sidekiq /etc/init.d
+chmod 755 --level/init.d/sidekiq
 
-sudo chkconfig --add sidekiq
-sudo chkconfig --level 345 sidekiq on
+chkconfig --add sidekiq
+chkconfig --level 345 sidekiq on
