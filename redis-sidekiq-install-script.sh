051485ae4acaabd15a258aa0a65f9fd7b214bb27
Bob Bonifield <bobbonifield@gmail.com>
2013-04-24 09:55:09 -0600

    Adding comments to script and minor cleanup

--- redis-sidekiq-install-script.sh
+++ redis-sidekiq-install-script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo yum -y update
-sudo yum -y install gcc gcc-c++ make 
-
+# install and setup redis
 sudo wget http://redis.googlecode.com/files/redis-2.6.12.tar.gz
 sudo tar xzf redis-2.6.12.tar.gz
 sudo rm redis-2.6.12.tar.gz -f
@@ -17,8 +15,9 @@ sudo cp redis.conf /etc/redis
 
 sudo sed -e "s/^daemonize no$/daemonize yes/" -e "s/^# bind 127.0.0.1$/bind 127.0.0.1/" -e "s/^dir \.\//dir \/var\/lib\/redis\//" -e "s/^loglevel verbose$/loglevel notice/" -e "s/^logfile stdout$/logfile \/var\/log\/redis.log/" redis.conf > /etc/redis/redis.conf
 
-wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
 
+# add redis as a service
+wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/redis-server
 sudo mv redis-server /etc/init.d
 sudo chmod 755 /etc/init.d/redis-server
 
@@ -27,12 +26,11 @@ sudo chkconfig --level 345 redis-server on
 
 sudo service redis-server start
 
-wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/sidekiq
 
+# add sidekiq as a service
+wget https://raw.github.com/messick/install-redis-amazon-linux-centos/master/sidekiq
 sudo mv sidekiq /etc/init.d
 sudo chmod 755 --level/init.d/sidekiq
 
 sudo chkconfig --add sidekiq
 sudo chkconfig --level 345 sidekiq on
-
-sudo service sidekiq start
